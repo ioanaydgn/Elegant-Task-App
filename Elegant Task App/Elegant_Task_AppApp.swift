a885444a55ac9ed6a2944e281430329a770e359a
Mert Aydogan <mertaydogn0@gmail.com>
2023-08-12 21:32:34 +0300

    Initial Commit

---
+++ Elegant Task App/Elegant_Task_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elegant_Task_AppApp.swift
+//  Elegant Task App
+//
+//  Created by Mert Aydoğan on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Elegant_Task_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
